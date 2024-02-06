65a46284fcc05e6d5b6d237d6b4e927a3334ec56
Ramesh Sundaramahalingam <rsundaramahaling@apple.com>
2024-02-06 19:35:59 +0530

    update css

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './App'
+import App from './App';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>
+    </BrowserRouter>
 );
 
 
