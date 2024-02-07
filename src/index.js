9564c88e4138a1f1b8d371052a2e2627dd57cd49
Ramesh Sundaramahalingam <rsundaramahaling@apple.com>
2024-02-07 12:48:44 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
 );
 
 
